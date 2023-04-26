3d085c31d7a094412f70e3953473554bfbc3b2f6
胡恩沛 <dmnph@login.speech>
2023-04-26 17:00:12 +0800

    Update scripts

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # Assignment-cross-lingual-transfer
 
-```bash=
-# get into fairseq/fairseq/modules/layer_norm.py
-# and comment out line 31 and 32
+# Assignment: Cross-lingual Transfer
 
-bash experiment/req.sh
-```
+## Prerequisites
+- Navigate to the file `fairseq/fairseq/modules/layer_norm.py` and comment out lines 31 and 32.
+- Run the script `experiment/req.sh`.
+	```bash=
+	bash experiment/req.sh
+	```
+
+## Script Descriptions
+- `experiment/test_translate_xnli.py`: Zero-shot translation from other languages to English. 
+  - TODO: Modify the script to perform k-shot translation with k > 0 to improve the translation quality. Examples for k-shot translation can be drawn from the dev set of XNLI since the other languages’ sentences are translations of the English ones. One can also use the dev set of the Flores-101 benchmark by MetaAI (https://dl.fbaipublicfiles.com/flores101/dataset/flores101_dataset.tar.gz).
+- `experiment/test_translate_xnli.py`: Zero-shot translation from other languages to English.
+  - TODO: Modify the script to perform k-shot translation with k > 0 to improve the translation quality. Examples for k-shot translation can be drawn from the dev set of XNLI since the other languages’ sentences are translations of the English ones. One can also use the dev set of the Flores-101 benchmark by MetaAI (https://dl.fbaipublicfiles.com/flores101/dataset/flores101_dataset.tar.gz).
+- `experiment/test_xnli.py`: Zero-shot in-context learning on the English testing set of XNLI.
+  - TODO: Modify the script to implement {0, 12}-shot in-context learning for part 1 and part 2.
+
--- experiment/test_xnli.py
+++ experiment/test_xnli.py
@@ -1,26 +1,49 @@
 from fairseq.models.transformer_lm import TransformerLanguageModel
 import numpy as np
 from tqdm import tqdm
+import os
 
 
+output_dir = 'output'
 model_dir = '7.5B'
+test_lang = 'en'
+output_name = f"zero_shot_{test_lang}.txt"
+
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+output_file = open(os.path.join(output_dir, output_name), 'w')
+
 lm = TransformerLanguageModel.from_pretrained(model_dir, bpe='sentencepiece')
 lm = lm.eval()
 lm = lm.half()
 lm = lm.cuda()
 
-def get_logprobs(prompt):
+def get_logprobs(prompt, verbose=False):
     import re
     prompt = re.sub('\n+' , '\n', prompt)  # collapse repeated newlines, which indicate separate documents
+    if verbose:
+        return prompt, lm.score(prompt, replace_newlines_with_eos=True)['positional_scores']
     return lm.score(prompt, replace_newlines_with_eos=True)['positional_scores']
     
 
 # to 
 pred_label = np.array(['entailment', 'contradiction', 'neutral'])
-def XNLI_eval(premise, hypothesis):
-    lprob1 = get_logprobs(premise + " , right? Yes, " + hypothesis).sum().cpu()
-    lprob2 = get_logprobs(premise + " , right? No, " + hypothesis).sum().cpu()
-    lprob3 = get_logprobs(premise + " , right? Also, " + hypothesis).sum().cpu()
+def XNLI_eval(premise, hypothesis, verbose=False):
+    if verbose:
+        prompt1, lprob1 = get_logprobs(premise + " , right? Yes, " + hypothesis, verbose)
+        prompt2, lprob2 = get_logprobs(premise + " , right? No, " + hypothesis, verbose)
+        prompt3, lprob3 = get_logprobs(premise + " , right? Also, " + hypothesis, verbose)
+        lprob1 = lprob1.sum().cpu()
+        lprob2 = lprob2.sum().cpu()
+        lprob3 = lprob3.sum().cpu()
+        output_file.write(prompt1+"\nScore: "+str(lprob1.item())+"\n")
+        output_file.write(prompt2+"\nScore: "+str(lprob2.item())+"\n")
+        output_file.write(prompt3+"\nScore: "+str(lprob3.item())+"\n\n")
+    else:
+        lprob1 = get_logprobs(premise + " , right? Yes, " + hypothesis, verbose).sum().cpu()
+        lprob2 = get_logprobs(premise + " , right? No, " + hypothesis, verbose).sum().cpu()
+        lprob3 = get_logprobs(premise + " , right? Also, " + hypothesis, verbose).sum().cpu()
     return pred_label[np.argmax([lprob1, lprob2, lprob3])]
     
 def val_all(premise, hypothesis):
@@ -49,16 +72,21 @@ label = np.array(label)
 premise = np.array(premise)
 hypothesis = np.array(hypothesis)
 
-en_ind = np.where(lang == 'en')
-en_label = label[en_ind]
-en_premise = premise[en_ind]
-en_hypothesis = hypothesis[en_ind]
+test_ind = np.where(lang == test_lang)
+test_label = label[test_ind]
+test_premise = premise[test_ind]
+test_hypothesis = hypothesis[test_ind]
 
 acc = 0.0
-for i in tqdm(range(len(en_label))):
-    predict = XNLI_eval(en_premise[i], en_hypothesis[i])
-    if predict == en_label[i]:
+for i in tqdm(range(len(test_label))):
+    predict = XNLI_eval(test_premise[i], test_hypothesis[i], verbose=(i%1000==0))
+    if predict == test_label[i]:
         acc += 1.0
 
-print('accuracy of zero-shot on en: ', acc/float(len(en_label)))
+
+print(f'accuracy of zero-shot on {test_lang}: ', acc/float(len(test_label)))
+output_file.write(f'accuracy of zero-shot on {test_lang}: '+str(acc/float(len(test_label))))
+output_file.write('\n')
+output_file.close()
+
 
